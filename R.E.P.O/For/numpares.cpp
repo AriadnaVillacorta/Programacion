dc457406a5bc6c2d575431642f9c7914eac46374
AriadnaVillacorta <00012725@uca.edu.sv>
2025-05-18 21:39:14 -0600

    Ejercicios

--- R.E.P.O/For/numpares.cpp
+++ R.E.P.O/For/numpares.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 
 int main (){
-int i, par, nm2;
+int i, par=0, nm2;
 
-cout<<"Numero spares del 1 al 50"<<endl;
+cout<<"Numeros pares del 1 al 50"<<endl;
 for (i =1; i <= 50; i++){
-
     if( i % 2 == 0){
         cout<< i <<endl;
-    for(par=++
+    par++;
+    
+    }
 } 
+  cout<<"Son " << par<< " numeros en total"<<endl;
 
-} 
 
 
 
